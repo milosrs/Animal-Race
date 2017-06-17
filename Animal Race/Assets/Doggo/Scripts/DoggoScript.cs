5c98a62a2faaedd5544db34ebc9b50d91e2dbd44
Milos <milos.tepic@yahoo.com>
2017-06-17 21:33:31 +0200

    Dog added

---
+++ Animal Race/Assets/Doggo/Scripts/DoggoScript.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoggoScript : MonoBehaviour {
+
+	private Rigidbody2D myRigidBody;
+
+	private Animator myAnimator;
+
+
+	[SerializeField]
+	private float movementSpeed;
+
+	private bool facingRight;
+
+	[SerializeField]
+	private Transform[] groundPoints;
+
+	[SerializeField]
+	private float groundRadius;
+
+	[SerializeField]
+	private LayerMask whatIsGround;
+
+	private bool isGrounded;
+
+	private bool jump;
+
+	[SerializeField]
+	private bool airControl;
+
+	[SerializeField]
+	private float jumpForce;
+
+
+	// Use this for initialization
+	void Start () {
+		facingRight = true;
+		myRigidBody = GetComponent<Rigidbody2D>();
+		myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis("Horizontal2");
+		isGrounded = IsGrounded();
+		HandleInput();
+		HandleMovement(horizontal);
+		Flip (horizontal);
+		ResetValues();
+	}
+
+	private void HandleMovement(float horizontal)
+	{
+		if (isGrounded || airControl)
+		{
+			myRigidBody.velocity = new Vector2 (horizontal * movementSpeed, myRigidBody.velocity.y);
+		}
+
+		if (isGrounded && jump)
+		{
+			isGrounded = false;
+			myRigidBody.AddForce(new Vector2(0, jumpForce));
+		}
+
+		//myRigidBody.velocity = new Vector2 (horizontal * movementSpeed, myRigidBody.velocity.y);
+		myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+	}
+
+	private void Flip(float horizontal)
+	{
+		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+		{
+			facingRight = !facingRight;
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+
+	private bool IsGrounded()
+	{
+		if (myRigidBody.velocity.y <= 0)
+		{
+			foreach (Transform point in groundPoints)
+			{
+				Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);	
+			
+				for (int i = 0; i < colliders.Length; i++)
+				{
+					if (colliders [i].gameObject != gameObject)
+					{
+						return true;
+					}
+
+				}
+			
+			}
+		}
+		return false;
+	}
+
+	private void HandleInput()
+	{
+		if(Input.GetKeyDown(KeyCode.I))
+			jump = true;
+	}
+
+	private void ResetValues()
+	{
+		jump = false;
+	}
+
+}
