718b32ff33bb91599af7a31e3e3c9cf5fa4b8344
milosrs <milosribar@yahoo.com>
2017-06-21 02:01:18 +0200

    Spawner script is better now

--- Animal Race/Assets/Scripts/SpawnerScript.cs
+++ Animal Race/Assets/Scripts/SpawnerScript.cs
@@ -7,15 +7,48 @@ public class SpawnerScript : MonoBehaviour {
     public GameObject[] obj;
     public float spawnMin = 10f;
     public float spawnMax = 20f;
+    private Rigidbody2D now;
+    private float lastX;
+    private bool canSpawn;
+    
 
-	// Use this for initialization
-	void Start () {
-        Spawn();
+    // Use this for initialization
+    void Awake () {
+        if (obj.Length > 1)
+        {
+            now = GetComponent<Rigidbody2D>();
+            lastX = now.position.x;
+            Spawn();
+            canSpawn = false;
+        }
+        else
+        {
+            Spawn();
+        }
 	}
-	
-	// Update is called once per frame
-	void Spawn() {
-        Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
-        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
-	}
+
+    private void FixedUpdate()
+    {
+        if (obj.Length > 1)
+        {
+            if (now.position.x > lastX && (now.position.x > lastX + 70))
+            {
+                Spawn();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Spawn() {
+        if (obj.Length > 1)
+        {
+            Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
+            lastX = now.position.x;
+        }
+        else
+        {
+            Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
+            Invoke("Spawn", 1.2f);
+        }
+    }
 }
